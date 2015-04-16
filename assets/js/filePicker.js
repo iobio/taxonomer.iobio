806b9fbfc8e9419c3398b841c8952468a2ba7b65
Chase Miller <chmille4@gmail.com>
2015-04-16 17:02:16 -0600

    add file download for url data sources

--- assets/js/filePicker.js
+++ assets/js/filePicker.js
@@ -7,18 +7,25 @@ function onFilesSelected(event) {
     var files = [];
     // convert to array
     var fileList = event.target.files;
+
+    // check for supported file types
+    var fileType0 = /[^.]+$/.exec(event.target.files[0].name)[0];
+    var fileType1 = /[^.]+$/.exec(event.target.files[1].name)[0];
+    if (fileType0 != 'fastq' & fileType0 != 'fq' & fileType0 != 'fasta' & fileType0 != 'fas' & fileType0 != 'fa') { alert('file must be a fasta(.gz) or fastq(.gz) file'); return; }
+    if (fileType1 != 'fastq' & fileType1 != 'fq' & fileType1 != 'fasta' & fileType1 != 'fas' & fileType1 != 'fa') { alert('file must be a fasta(.gz) or fastq(.gz) file'); return; }
+    
     for (var i=0; i < fileList.length; i++) { files.push(fileList[i]);}
     // analyze
 		goFile(files)
 	
 	} else if (event.target.files.length == 1) {
 		// read fastq
-		var fileType0 = /[^.]+$/.exec(event.target.files[0].name)[0];
-		if (fileType0 != 'fastq') { alert('file must be a .fastq file'); return; }
+		var fileType0 = /[^.]+$/.exec(event.target.files[0].name)[0];    
+		if (fileType0 != 'fastq' & fileType0 != 'fq' & fileType0 != 'fasta' & fileType0 != 'fas' & fileType0 != 'fa') { alert('file must be a fasta(.gz) or fastq(.gz) file'); return; }
 		var file = event.target.files[0];
 		goFile([file]);
 	} else {
-		alert('select 1 fastq file or 2 fastq files that are mate pairs')
+		alert('select 1 fastq/fasta file or 2 fastq/fasta files that are mate pairs')
 	}
 }
 
